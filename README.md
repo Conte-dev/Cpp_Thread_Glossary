0c6bf6f48f7918bf24e3e80f7c5db86fadc59625
Conte-Developer <samueledone08@gmail.com>
2025-10-19 15:19:57 +0200

    creazione del file readme.md, primo esempio di due thread concorrenti

---
+++ README.md
@@ -0,0 +1,5 @@
+# GLOSSARIO DI C++ (Utilizzo  thread e programmazione concorrente)
+in questo repositories è possibile trovare tutte le nozioni che ho potuto acquisire sull' uso dei Thread.
+In riferimento alla gestione di codici C++ che prevedono problematiche di sincronizzazione di processi.
+
+## 1. GESTION BASE DI UN THREAD 
--- thread_example.cpp
+++ thread_example.cpp
@@ -1,4 +1,16 @@
 #include<iostream>
 #include<thread>
+using namespace std;
+
+void CreaThread(){                        
+    cout<<"secondo thread in esecuzione \n";
+}
+int main(){
+    thread t(CreaThread);
+    cout<<"primo rthread in esecuzione \n";
+    t.join();  //aspetta che il thread creato dalla funzione finisca l'esecuzione
+    cout<<"secondo thread terminato \n";
+    return 0; 
+}
 
 
